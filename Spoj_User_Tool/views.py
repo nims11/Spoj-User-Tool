628dbd96703bb93adfa34d9e21d0b92b702b8d57
Nimesh Ghelani <nimeshghelani@gmail.com>
2012-12-24 22:09:10 +0530

    Problem Ids are now shown as links

--- Spoj_User_Tool/views.py
+++ Spoj_User_Tool/views.py
@@ -11,15 +11,15 @@ def home(request):
 
 def get_user_info(user_name):
 	#html = urllib2.urlopen('http://www.spoj.pl/users/'+user_name).read()
-	#html = urllib2.urlopen('http://localhost/'+user_name+'.htm').read()
-	html = urlfetch.fetch('http://www.spoj.pl/users/'+user_name, deadline=30).content
+	html = urllib2.urlopen('http://localhost/'+user_name+'.htm').read()
+	#html = urlfetch.fetch('http://www.spoj.pl/users/'+user_name, deadline=30).content
 	html = html.replace('\n','')
 	
 	user_info = user_page_parse(html, user_name)
 	# parsing signed list
 	#signed_html = urllib2.urlopen('http://www.spoj.pl/status/'+user_name+'/signedlist/').read()
-	#signed_html = urllib2.urlopen('http://localhost/'+user_name+'signedlist').read()
-	signed_html = urlfetch.fetch('http://www.spoj.pl/status/'+user_name+'/signedlist/', deadline=30).content
+	signed_html = urllib2.urlopen('http://localhost/'+user_name+'signedlist').read()
+	#signed_html = urlfetch.fetch('http://www.spoj.pl/status/'+user_name+'/signedlist/', deadline=30).content
 	problem_info = signed_list_parse(signed_html, user_info)
 	user_info.probPool.calc_stats()
 	user_info.get_classical_table()
