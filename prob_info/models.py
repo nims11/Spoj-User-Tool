5d4e46bac841147f0a32adf02f390c4bbf7460aa
Nimesh Ghelani <nimeshghelani@gmail.com>
2015-05-23 23:44:26 +0530

    fixed solved by

--- prob_info/models.py
+++ prob_info/models.py
@@ -5,6 +5,7 @@ import urllib2
 import logging
 import pickle
 import base64
+from lxml import html as xpathDoc
 from google.appengine.api import urlfetch
 regex = re.compile(r'<td><a href="/ranks/([A-Z0-9_]+)" title="Value [0-9]+\.?[0-9]* points. See the best solutions.">([0-9]+)</a></td>')
 last_regex = re.compile(r'<a href="/problems/classical/sort=0,start=([0-9]+)" class="pager_link">&gt;</a>')
@@ -18,32 +19,30 @@ class ProblemClassical(models.Model):
 
 	data = property(get_data, set_data)
 
-
-def parsePage(html, tar_dict):
-	res = regex.finditer(html)
-	for match in res:
-		tar_dict[match.group(1)] = match.group(2)
-		logging.info(match.group(1) + ' ' + match.group(2))
+def getNextUrl(contentTree):
+    try:
+        urls = contentTree.xpath('//table[@class="navigation"]//a[text()="Next"]/@href')
+    except:
+        return False
+    if len(urls) == 0:
+        return False
+    return 'http://www.spoj.com'+urls[0]
 
 def iterPages():
 	res = {}
-	prefix = 'http://www.spoj.com/problems/classical/sort=0,start='
-	html = urlfetch.fetch(prefix+'0', deadline=30).content
-	parsePage(html, res)
-	end = 0
-	match = last_regex.search(html)
-	if match:
-		end = int(match.group(1))
-	step = 50
-	curr = step
-	
-	logging.info('Going through problem pages, curr = %d, step = %d, end = %d' % (curr, step, end))
-	for i in xrange(curr, end+1, step):
-		html = urlfetch.fetch(prefix+str(i), deadline=30).content
-		parsePage(html, res)
-	
-	ProblemClassical.objects.all().delete()
-	ProblemClassical(data=pickle.dumps(res)).save()
+	url = 'http://www.spoj.com/SPOJ/problems/classical/'
+        while url != False:
+            logging.info('URL: '+url)
+            html = urlfetch.fetch(url, deadline=30).content
+            doc = xpathDoc.fromstring(html)
+            for row in doc.xpath('//tr[@class="problemrow"]'):
+                pcode = row.xpath('./td[3]/a/text()')[0].strip()
+                users = row.xpath('./td[4]/a/text()')[0].strip()
+                res[pcode] = users
+                logging.info(pcode+': '+users)
+            url = getNextUrl(doc)
+        ProblemClassical.objects.all().delete()
+        ProblemClassical(data=pickle.dumps(res)).save()
 
 def gen(request, x):
 	iterPages()
